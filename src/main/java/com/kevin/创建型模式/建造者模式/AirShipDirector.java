9c5e0f76e077679f20114030a732614ed83ce2c2
kevin961212 <1021979964@qq.com>
2019-02-18 14:11:10 +0800

    建造者模式和UML类图

---
+++ src/main/java/com/kevin/创建型模式/建造者模式/AirShipDirector.java
@@ -0,0 +1,14 @@
+package com.kevin.创建型模式.建造者模式;
+
+/**
+ * @author kevin
+ * @version 1.0
+ * @description     装配类
+ * @createDate 2019/2/17
+ */
+public interface AirShipDirector {
+
+    // 组装飞船对象
+    AirShip directorAirShip();
+
+}
---
+++ src/main/java/com/kevin/创建型模式/建造者模式/SpaceAirShipDirector.java
@@ -0,0 +1,31 @@
+package com.kevin.创建型模式.建造者模式;
+
+/**
+ * @author kevin
+ * @version 1.0
+ * @description     太空飞船装配类
+ * @createDate 2019/2/17
+ */
+public class SpaceAirShipDirector implements AirShipDirector{
+
+    private AirShipBuilder builder;
+
+    public SpaceAirShipDirector(AirShipBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public AirShip directorAirShip() {
+        Engine e = builder.builderEngine();
+        OrbitalModule o = builder.builderOrbitalModule();
+        EscapeTower et = builder.builderEscapeTower();
+
+        // 装配成飞船
+        AirShip ship = new AirShip();
+        ship.setEngine(e);
+        ship.setOrbitalModule(o);
+        ship.setEscapeTower(et);
+
+        return ship;
+    }
+}
